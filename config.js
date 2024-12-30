cacf8d3cd681e608cb87cb9482d4c8419cc6d323
baimdevs <baimstore71@gmail.com>
2024-12-30 13:12:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   BOT_TOKEN: "7876341720:AAF8C7ZtmvPLPzZXtiCxHrKhAKxQSLJg90Q", // Token bot Telegram
-  OWNER_ID: "6552202106", // ID pemilik bot
+  OWNER_ID: "7866515797", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
